81c94ef3d6c98271a5cabf99c0292027a11da8fa
bee <3172736837@qq.com>
2025-03-12 21:56:32 +0800

    完善细节、预算更新

---
+++ BudgetsAPP/Views/BudgetSummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  BudgetSummaryView.swift
+//  BudgetsAPP
+//
+//  Created by 珠穆朗玛小蜜蜂 on 2025/3/12.
+//
+
+import SwiftUI
+
+struct BudgetSummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BudgetSummaryView()
+}
---
+++ BudgetsAPP/Views/TransactionListView.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionListView.swift
+//  BudgetsAPP
+//
+//  Created by 珠穆朗玛小蜜蜂 on 2025/3/12.
+//
+
+import SwiftUI
+
+struct TransactionListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionListView()
+}
